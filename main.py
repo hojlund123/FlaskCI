7fbd53b3dd55015da97070ebf695302633dcfb77
Emil Højlund Larsen <hojlund123@users.noreply.github.com>
2024-06-11 22:20:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import time
 
 # Set the FLASK_APP environment variable
-os.environ['FLASK_APP'] = 'web.app:create_app()'
+os.environ['FLASK_APP'] = 'web.app:create_app'
 
 app = create_app()
 
@@ -13,17 +13,13 @@ if __name__ == '__main__':
     start_worker()  # Start the job worker
 
     # Run the Flask app in a subprocess
-    app_process = subprocess.Popen(["flask", "run", "--port", "5000"])
+    app_process = subprocess.Popen(["flask", "run", "--host", "0.0.0.0", "--port", "5000"])
 
     # Expose the Flask app using Localtunnel
-    while True:
-        try:
-            result = subprocess.run(["lt", "--port", "5000", "--subdomain", "larslars1337"], check=True)
-            break
-        except subprocess.CalledProcessError as e:
-            print(f"Localtunnel connection failed: {e}")
-            print("Retrying in 5 seconds...")
-            time.sleep(5)
-        except KeyboardInterrupt:
-            app_process.terminate()
-            break
+    try:
+        result = subprocess.run(["lt", "--port", "5000", "--subdomain", "larslars1337"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Localtunnel connection failed: {e}")
+        app_process.terminate()
+    except KeyboardInterrupt:
+        app_process.terminate()
