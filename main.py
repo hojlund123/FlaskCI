1e96e307a8235776769fdfc4caffbe7a03563e26
Emil Højlund Larsen <hojlund123@users.noreply.github.com>
2024-06-11 18:14:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from web.app import create_app
 from utils.job import start_worker
+import subprocess
 
 app = create_app()
 
 if __name__ == '__main__':
     start_worker()  # Start the job worker
-    app.run(port=5000)
+
+    # Run the Flask app
+    app_process = subprocess.Popen(["python", "-m", "flask", "run", "--port", "5000"])
+
+    # Expose the Flask app using Localtunnel
+    try:
+        subprocess.run(["lt", "--port", "5000", "--subdomain", "larslars1337"])
+    except KeyboardInterrupt:
+        app_process.terminate()
